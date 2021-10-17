71878e1709539ff6b00c8b0cfcc74e6c47d969fc
Jahangir-Alam-Noman <jahangir2km@gmail>
2021-10-18 02:36:17 +0600

    not found added

---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NotFound.css';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>Hello I am from Not Found </h2>
+
+        </div>
+    );
+};
+
+export default NotFound;
