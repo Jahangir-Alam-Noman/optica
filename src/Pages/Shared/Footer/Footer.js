1aaa692423aca70c559dcdee879f8ca1a7997845
Jahangir-Alam-Noman <jahangir2km@gmail>
2021-10-18 02:24:45 +0600

    first commit

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div>
+            <h2> Hello Iam From Footer</h2>
+
+
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Hello I am from header</h2>
+
+        </div>
+    );
+};
+
+export default Header;
